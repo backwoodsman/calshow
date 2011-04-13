62cc67a643fc52504e3b3a211875738d9ff75eff
Richard Lyons <richard@co-ho.net>
2011-04-13 12:52:25 +0100

    tidy up and add INSTALL etc

---
+++ lib/lang/calshow/ext/it_IT.php
@@ -0,0 +1,25 @@
+<?php
+$lang['day_short1'] = 'lun';
+$lang['day_short2'] = 'mar';
+$lang['day_short3'] = 'mer';
+$lang['day_short4'] = 'gio';
+$lang['day_short5'] = 'ven';
+$lang['day_short6'] = 'sab';
+$lang['day_short7'] = 'dom';
+$lang['month1'] = 'genaio';
+$lang['month2'] = 'febraio';
+$lang['month3'] = 'marzo';
+$lang['month4'] = 'aprile';
+$lang['month5'] = 'maggio';
+$lang['month6'] = 'giugno';
+$lang['month7'] = 'luglio';
+$lang['month8'] = 'agosto';
+$lang['month9'] = 'settembre';
+$lang['month10'] = 'ottobre';
+$lang['month11'] = 'novembre';
+$lang['month12'] = 'dicembre';
+$lang['legend1'] = 'legend';
+$lang['legend2'] = 'available';
+$lang['legend3'] = 'not available';
+$lang['legend4'] = 'stronger tones show weekend rates';
+?>
